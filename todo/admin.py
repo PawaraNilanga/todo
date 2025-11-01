d7c6bdb3573acc3bc800f0fe5d063d8f3dfcc2f9
Pawara Nilanga <pawara.rox@gmail.com>
2025-11-01 17:29:32 +0530

    Todo app Final

--- todo/admin.py
+++ todo/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Task
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('task','is_completed', 'updated_at')
+    list_display = ('task','is_completed', 'updated_at', 'created_at')
     search_fields = ('task',)
 
 admin.site.register(Task, TaskAdmin)
--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
+from pyexpat import features
+
 from . import views
 
 urlpatterns = [
-
+    # add Task
     path('addTask/', views.addTask, name='addTask'),
+
+    # mark as done
     path('mark_as_done/<int:pk>/',views.mark_as_done, name='mark_as_done'),
+
+    # mark as un done
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone, name='mark_as_undone'),
+
+    # edit features
+    path('edit_task/<int:pk>/',views.edit_task, name='edit_task'),
+
+    # Delete Task
+    path('delete_task/<int:pk>/',views.delete_task, name='delete_task'),
 ]
